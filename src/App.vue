2a858f8a740f2015a8fcd4e9fba7c63bfb43c3b8
letícia <leticiaborgess.oficial@gmail.com>
2022-04-25 10:42:25 -0300

    Exercicio em sala

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script>
 import MenuLateral from "@/components/MenuLateral.vue";
+import CaBeçalho from "@/components/CaBeçalho.vue";
+import ROdape from "@/components/ROdape.vue";
+import BlocoPrincipal from "@/components/BlocoPrincipal.vue";
+
 export default {
-  components: { MenuLateral },
+  components: { MenuLateral, CaBeçalho, ROdape, BlocoPrincipal },
 };
 </script>
 <template>
   <MenuLateral />
-  <header id="header">Cabeçalho</header>
-  <main id="content">Principal</main>
-  <footer id="footer">Rodapé</footer>
+  <CaBeçalho />
+  <ROdape />
+  <BlocoPrincipal />
 </template>
 <style></style>
---
+++ src/components/BlocoPrincipal.vue
@@ -0,0 +1,16 @@
+<script>
+export default {};
+</script>
+<template>
+  <main id="content">
+    <article class="block">Olá mundo</article>
+    <article class="block">Olá mundo</article>
+    <article class="block">Olá mundo</article>
+    <article class="block">Olá mundo</article>
+    <article class="block">Olá mundo</article>
+    <article class="block">Olá mundo</article>
+    <article class="blockead">Olá mundo</article>
+  
+  </main>
+</template>
+<style></style>
